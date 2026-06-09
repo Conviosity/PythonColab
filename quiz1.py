6528219e71710826adcacd147f0ef7658bce3131
Conviosity <collin.seipel9@gmail.com>
2026-06-09 21:57:29 +0200

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -2,7 +2,9 @@ import ipywidgets as widgets
 from IPython.display import display, clear_output
 
 def starte_quiz():
-    optionen = widgets.RadioButtons(options=['Der Computer druckt das Wort Hallo auf Papier aus.', 'Es gibt eine Fehlermeldung, weil der Computer "Hallo" für einen Befehl hält, den er nicht kennt.', 'Nichts.'], value=None, description='Antworten:')
+    optionen = widgets.RadioButtons(options=['Der Computer druckt das Wort Hallo auf Papier aus.',
+                                             'Es gibt eine Fehlermeldung, weil der Computer "Hallo" für einen Befehl hält, den er nicht kennt.',
+                                             'Nichts.'], value=None, description='Antworten:')
     button = widgets.Button(description="Prüfen", button_style='info')
     ausgabe = widgets.Output()
 
